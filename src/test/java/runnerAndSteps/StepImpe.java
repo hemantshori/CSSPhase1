c96db6cfd483909daf004a6921ad58f69e915d41
hemant shori <hemantshori@gmail.com>
2017-01-18 08:44:40 +1100

    enabling close browser

--- src/test/java/runnerAndSteps/StepImpe.java
+++ src/test/java/runnerAndSteps/StepImpe.java
@@ -51,10 +51,10 @@ public class StepImpe {
 	}
 		// **************disable to leave browser open***************************************
 
-//	@After()
-//		  public void tearDown() {	
-//		    driver.quit();
-//		   	  }
+	@After()
+		  public void tearDown() {	
+		    driver.quit();
+		   	  }
 	//******************************************************************************   
 	    
    
